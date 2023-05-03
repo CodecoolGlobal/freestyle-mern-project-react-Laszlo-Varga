d61424027308d1928435228aad6fec887d2e5a21
gaborati <113521931+gaborati@users.noreply.github.com>
2023-05-03 10:26:12 +0200

    display characters and add button

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,57 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import md5 from "md5";
+import DisplayCharacters from "./Components/DisplayCharacters";
+function MarvelCharacters() {
+  const [characters, setCharacters] = useState([]);
+  const [character, setCharacter] = useState([]);
+  useEffect(() => {
+    const ts = new Date().getTime(); 
+    const privateKey = "666b92b8ab04d83da5d59922b7d5d3611b6bd393";
+    const publicKey = "cc0343e9c73f6dead5ff80ea4654be13";
+    const hash = md5(ts + privateKey + publicKey);
 
-function App() {
+    const [timestamp, apiKey, hashValue] = [ts, publicKey, hash];
+
+    const url = `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}`;
+
+    async function getCharacters(url) {
+      const charactersRes = await fetch(url);
+      const charactersArray = await charactersRes.json();
+      setCharacters(charactersArray.data.results);
+      console.log(characters);
+    }
+    getCharacters(url);
+  }, []);
+
+  console.log(characters);
+const handleCharacterClick = (character) => {
+setCharacter(character)
+console.log(character)
+}
+
+
+fetch("http://localhost:3000/heroes",{
+  method : "Post",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(character),
+})
+.then((response) => {
+  if(response.ok){
+    return response.json()
+  }
+  throw new Error("Something went wrong")
+})
+.then (data => console.log(data))
+.catch(err => console.log(err))
+
+
   return (
-    <div className="App">
-      
+    <div>
+      <DisplayCharacters characters={characters} 
+      handleCharacterClick={handleCharacterClick}/>
     </div>
   );
 }
 
-export default App;
+
+export default MarvelCharacters;
---
+++ client/src/Components/DisplayCharacters.js
@@ -0,0 +1,22 @@
+import {React} from "react";
+ 
+function DisplayCharacters({characters,handleCharacterClick}){
+
+return (
+
+ <div>
+      {characters.map((character) => (
+        <div key={character.id}>
+          <h3>{character.name}</h3>
+          <img
+            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            alt={character.name}
+          />
+        <button onClick={() => handleCharacterClick(character)}>Add to Favorites</button>
+        </div>
+      ))}
+    </div>
+
+    )}
+
+export default DisplayCharacters;
